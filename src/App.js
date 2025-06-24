2172b413754cef4f0108ee270d9d6174139ec6c7
MariyaSemiryazhko <semiryazhkomasha@gmail.com>
2025-06-24 22:07:16 +0200

    update footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
        Hello
       </header>
+      <main></main>
+      <footer> <a href="https://github.com/MariyaSemiryazhko/dictionary-app"> Open-sourced </a> on <a href="https://rainbow-strudel-171435.netlify.app/">Netlify </a> </footer>
     </div>
   );
 }
